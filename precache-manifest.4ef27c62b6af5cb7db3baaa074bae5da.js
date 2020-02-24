720b08b788f3797ed5ea443563f94945d8eef821
Mr. Rabbit <oldwhite37201@users.noreply.github.com>
2020-02-24 15:05:57 +0700

    Updates

---
+++ precache-manifest.4ef27c62b6af5cb7db3baaa074bae5da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf7f7f16ffa4ae0208c565d6da0bb960",
+    "url": "/randomquote/index.html"
+  },
+  {
+    "revision": "452a0f4a370794c2b20e",
+    "url": "/randomquote/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "957a81aed0b156ea6363",
+    "url": "/randomquote/static/js/2.4c4c0ec0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/randomquote/static/js/2.4c4c0ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "452a0f4a370794c2b20e",
+    "url": "/randomquote/static/js/main.2e7c6f1d.chunk.js"
+  },
+  {
+    "revision": "e96de22e9f6ed6c59e44",
+    "url": "/randomquote/static/js/runtime-main.5d75b14d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/randomquote/static/media/logo.5d5d9eef.svg"
+  }
+]);
